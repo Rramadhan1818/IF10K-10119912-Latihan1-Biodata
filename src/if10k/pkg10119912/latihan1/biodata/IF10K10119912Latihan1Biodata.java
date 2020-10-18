9e230fcbf1c7fae19ee3cafd72f6fd2e7316c19f
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-10-18 15:21:33 +0700

    First commit

---
+++ src/if10k/pkg10119912/latihan1/biodata/IF10K10119912Latihan1Biodata.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * 
+ */
+public class IF10K10119912Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA MAHASISWA" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Rezki Ramadhan");
+        System.out.println("ALAMAT\t\t: Jalan Cihaur no A10");
+        System.out.println("TEMPAT LAHIR\t: Purwakarta");
+        System.out.println("TANGGAL LAHIR\t: 18 Desember 2000");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Ngoding & Main Basket");
+        System.out.println("MAKES\t\t: Nasi Padang");
+        System.out.println("MIKES\t\t: Teh");        
+        
+    }
+    
+}
